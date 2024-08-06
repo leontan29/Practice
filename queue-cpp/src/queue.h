9760a97b347c1c06bf6badc83abc2895bcc3fe61
CK Tan <cktanx@gmail.com>
2024-08-05 17:41:04 -0700

    fix

--- queue-cpp/src/queue.h
+++ queue-cpp/src/queue.h
@@ -4,6 +4,7 @@
 #include <vector>
 
 
+// A circular queue backed by vector.
 template <typename T> class Queue {
 private:
   size_t _head;  // _data[_head] is first elem
@@ -12,13 +13,35 @@ private:
   std::vector<T> _data; // backing array
 
 public:
-  Queue(size_t capacity);
+  // Create a queue that can fit N members (the capacity).
+  Queue(size_t N);
+
+  // Check if queue is empty.
   bool is_empty() const;
+
+  // Check if queue is full.
+  bool is_full() const;
+  
+  // Return the #elem in queue.
   size_t size() const;
+
+  // Look at the head. Return false if queue is empty.
   bool peek(T& elem) const;
+
+  // Put elem into queue.
   bool enqueue(const T &elem);
+
+  // Remove the elem at head. Return false if queue is empty.
   bool dequeue();
-  void resize(size_t capacity);
+
+  // Change the capacity of the queue to N. Drop elems from head of
+  // queue if new capacity is smaller than current queue size.
+  void resize(size_t N);
+
+  // Return a string '[x y z]' if queue contains x, y, z.  If there
+  // are more than limit items in queue, return a string of limit
+  // items with ellipses at the end.  Assume T has anoverloaded
+  // ostream& operator<<() function.
   std::string to_string(size_t limit=10) const;
 
   // Don't remove this line
