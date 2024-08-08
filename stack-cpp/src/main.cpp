7fd862582fb0e8d1e78d126cf530665b1fce8a60
Leon Tan <leontan29@gmail.com>
2024-08-08 15:20:36 -0700

    finish

--- stack-cpp/src/main.cpp
+++ stack-cpp/src/main.cpp
@@ -1,4 +1,4 @@
-#include "queue.h"
+#include "stack.h"
 #include <iostream>
 
 
@@ -16,15 +16,95 @@ static inline void check(bool flag, const char *file, int line) {
 class Tests {
 public:
   static void test1();
+  static void test2();
+  static void test3();
+  static void test4();
 };
 
 void Tests::test1() {
+  cout << "Test 1: Constructor" << endl;
+  Stack<int> s(10);
+  CHECK(s.is_empty());
+  CHECK(!s.is_full());
+}
+
+void Tests::test2() {
+  {
+    cout << "Test 2: Push & Pop" << endl;
+    Stack<int> s(10);
+    CHECK(s.size() == 0);
+    for (int i = 0; i < 10; i++) {
+      s.push(i);
+    }
+    CHECK(s.is_full());
+    CHECK(s.size() == 10);
+  }
+
+  {
+    Stack<int> s(0);
+    // push 100 things
+    for (int i = 0; i < 100; i++) {
+      s.push(i);
+    }
+    // make sure the capacity grows
+    for (int i = 99; i >= 50; i--) {
+      int result;
+      CHECK(s.peek(result) && result == i && s.pop());
+    }
+    
+    for (int i = 1000; i <= 2000; i++) {
+      s.push(i);
+    }
+    
+    for (int i = 2000; i >= 1000; i--) {
+      int result;
+      CHECK(s.peek(result) && result == i && s.pop());
+    }
+    
+    for (int i = 49; i >= 0; i--) {
+      int result;
+      CHECK(s.peek(result) && result == i && s.pop());
+    }
+    
+    CHECK(s.size() == 0);
+  }
+}
+
+void Tests::test3() {
+  cout << "Test 3: Peek" << endl;
+  Stack<int> s(10);
+  CHECK(s.size() == 0);
+  for (int i = 0; i < 10; i++) {
+    s.push(i);
+  }
+
+  int result;
+  CHECK(s.peek(result) && result == 9 && s.pop());
+  CHECK(s.peek(result) && result == 8 && s.pop());
+  CHECK(s.peek(result) && result == 7 && s.pop());
+}
+
+void Tests::test4() {
+  cout << "Test 4: to_string" << endl;
+  Stack<int> s(10);
+  CHECK(s.size() == 0);
+  CHECK(s.to_string() == "[]");
+  
+  for (int i = 0; i < 10; i++) {
+    s.push(i);
+  }
+  cout << s.to_string() << endl;
+  CHECK(s.to_string() == "[9 8 7 6 5 4 3 2 1 0]");
+  
 }
 
 int main()
 {
   try {
     Tests::test1();
+    Tests::test2();
+    Tests::test3();
+    Tests::test4();
   } catch (const std::string &ex) {
     cout << ex << "\n";
     exit(1);
--- stack-cpp/src/stack.h
+++ stack-cpp/src/stack.h
@@ -9,7 +9,8 @@ template <typename T> class Stack {
 private:
   size_t _top; // num of valid elem in _data[]
   std::vector<T> _data; // backing array
-
+  void grow(); //increase by 20 percent
+  void check();
 public:
   // Create a stack that can fit N members (the capacity) initially.
   // The capacity should be increased automatically, by push(), when
@@ -29,7 +30,7 @@ public:
   bool peek(T& elem) const;
 
   // Put elem into stack.
-  bool push(const T &elem);
+  void push(const T &elem);
 
   // Remove the elem at the top. Return false if stack is empty.
   bool pop();
@@ -41,7 +42,89 @@ public:
   friend class Tests;
 };
 
+template<typename T>
+void Stack<T>:: check() {
+  if (!(_top <= _data.size())) {
+    throw std::runtime_error("internal error");
+  }
+}
 
 //TODO: implement the methods above
-template <typename T> Stack::Stack(size_t N) { // fill in 
+template <typename T>
+Stack<T>::Stack(size_t N) : _top(0), _data(N) { }
+
+// Check if stack is empty.
+template <typename T>
+bool Stack<T>::is_empty() const {
+  return _top == 0;
+}
+
+// Check if stack is full.
+template<typename T>
+bool Stack<T>:: is_full() const {
+  return _top == _data.size();
+}
+
+template<typename T>
+void Stack<T>:: grow() {
+  auto N = (_data.size() + 1);
+  N *= 1.2;
+  _data.resize(N);
+}
+
+// Return the #elem in stack.
+template<typename T>
+size_t Stack<T>:: size() const {
+  return _top;
+}
+
+// Put elem into stack.
+template<typename T>
+void Stack<T>:: push(const T &elem) {
+  check();
+  // add element to front of the stack
+  if (is_full()) {
+    grow();
+  }
+  _data[_top++] = elem;
+  check();
+}
+
+// Remove the elem at the top. Return false if stack is empty.
+template<typename T>
+bool Stack<T>:: pop() {
+  check();
+  if (is_empty()) {
+    return false;
+  }
+  _top--;
+  return true;
+}
+
+
+// Look at the top. Return false if stack is empty.
+template<typename T>
+bool Stack<T>:: peek(T& elem) const {
+  if (is_empty()){
+    return false;
+  }
+  elem = _data[_top - 1];
+  return true;
+}
+
+// Return a string '[z y x]' if stack contains x, y, z.
+template<typename T>
+std::string Stack<T>::to_string() const {
+  std::ostringstream buf;
+  buf << "[";
+  bool printspace = false;
+  for (int i = (int)_top - 1; i >= 0; i--) {
+    if (printspace) {
+      buf << " ";
+    }
+    printspace = true;
+    buf << _data[i];
+  }
+  buf << "]";
+  return buf.str();
 }
